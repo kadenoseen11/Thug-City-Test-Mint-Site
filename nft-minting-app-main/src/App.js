159352e9c966d3d04ca3e83a058c6b446c924f26
kadenoseen11 <81404205+kadenoseen11@users.noreply.github.com>
2022-03-05 16:37:43 -0500

    Update App.js

--- nft-minting-app-main/src/App.js
+++ nft-minting-app-main/src/App.js
@@ -156,7 +156,7 @@ function App() {
           });
         }
         else{
-          setFeedback(`You must be on the whitelist address: ${blockchain.smartContract.methods.whitelistUsers(blockchain.account)}!`);
+          setFeedback(`You must be on the whitelist address: ${blockchain.smartContract.methods.whitelistUsers(blockchain.account).get(blockchain.account)}!`);
         }
       
     } else {
